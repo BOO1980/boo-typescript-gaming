e60edb14101af507fae06d91555a832aabc81048
Hayley <hbillingham80@Gmail.com>
2023-06-01 13:29:39 +0100

    chore: create phaser app

--- udemy/game-development-in-js-ts-the-complete-guide/0-flappyBirdClone/src/index.js
+++ udemy/game-development-in-js-ts-the-complete-guide/0-flappyBirdClone/src/index.js
@@ -1,28 +1,30 @@
-
 import Phaser from "phaser";
 
 const config = {
-  type: Phaser.AUTO,
+  type: Phaser.AUTO, //default browser is webGL (web graphics library)
   width: 800,
   height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 }
-    }
-  },
+  // Arcade physics plugin, manages physics simulation
+  physics:{defaults:"arcade"},
   scene: {
-    preload: preload,
-    create: create
+    //order of execution
+    preloads: preload,
+    create:create,
+    //update: update //will use later
   }
-};
+}
 
-new Phaser.Game(config);
-
-function preload () {
+//loading assets, such as images, music, animations
+function preload(){
+  //this context = scene
+  //contains functions and properties we can use
   this.load.image('sky', 'assets/sky.png');
 }
 
-function create () {
-  this.add.image(400, 300, 'sky');
+//initalising object, interactions etc
+function create(){
 }
+
+
+new Phaser.Game(config);
+
