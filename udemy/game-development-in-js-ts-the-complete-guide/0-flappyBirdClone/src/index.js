397a58beff8a879c0a6ad44f0ae83bcecab03bdd
Hayley <hbillingham80@Gmail.com>
2023-06-01 13:57:14 +0100

    chore: display image

--- udemy/game-development-in-js-ts-the-complete-guide/0-flappyBirdClone/src/index.js
+++ udemy/game-development-in-js-ts-the-complete-guide/0-flappyBirdClone/src/index.js
@@ -7,8 +7,7 @@ const config = {
   // Arcade physics plugin, manages physics simulation
   physics:{defaults:"arcade"},
   scene: {
-    //order of execution
-    preloads: preload,
+    preload: preload,
     create:create,
     //update: update //will use later
   }
@@ -18,11 +17,14 @@ const config = {
 function preload(){
   //this context = scene
   //contains functions and properties we can use
-  this.load.image('sky', 'assets/sky.png');
+  this.load.image('sky','assets/sky.png');
 }
 
 //initalising object, interactions etc
 function create(){
+
+  //this.add.image(x,y,'keyname');
+  this.add.image(config.width/2,config.height/2,'sky');
 }
 
 
